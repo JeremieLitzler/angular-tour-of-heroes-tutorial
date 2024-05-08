dffea34a5a9454fc93315371227273adae4f3031
Jeremie Litzler <jeremie.litzler@gmail.com>
2024-05-08 14:28:34 +0200

    complete The hero editor and Display a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,21 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { Hero } from '../../models/hero';
+import { HEROES } from '../../sample-data/mock-heroes';
 
 @Component({
+  // standalone: true,
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrl: './heroes.component.css',
+  // imports: [NgFor],
 })
 export class HeroesComponent {
+  heroes: Hero[] = HEROES;
 
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    console.log(hero);
+    this.selectedHero = hero;
+  }
 }
